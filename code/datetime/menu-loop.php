aa23f7ecae0d88cbb5ccf42e9fe3c30500ac623b
Enjoy <kanneg881@gmail.com>
2018-06-26 10:07:29 +0800

    Learning_PHP/code/datetime 重構

--- code/datetime/menu-loop.php
+++ code/datetime/menu-loop.php
@@ -1,10 +1,15 @@
 <?php
+/** @var int $daysToPrint 印出天數 */
 $daysToPrint = 4;
-$d = new DateTime('next Tuesday');
+/** @var DateTime $datetime 時間物件 */
+$datetime = new DateTime('next Tuesday');
+
 print "<select name='day'>\n";
+
 for ($i = 0; $i < $daysToPrint; $i++) {
-    print "  <option>" . $d->format('l F jS') . "</option>\n";
-    // Add 2 days to the date
-    $d->modify("+2 day");
+    print "  <option>" . $datetime->format('l F jS') . "</option>\n";
+    // 從現在的日期增加兩天
+    $datetime->modify("+2 day");
 }
+
 print "</select>";
